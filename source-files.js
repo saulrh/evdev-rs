bffec3be034932da810f62d9cec8ff1cae780cfb
ci <ci>
2021-12-22 15:07:30 +0000

    init

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["evdev_rs"] = {"name":"","files":["device.rs","enums.rs","lib.rs","logging.rs","macros.rs","uinput.rs","util.rs"]};
+createSourceSidebar();
